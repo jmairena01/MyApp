2164397eb8de8b9005f8a8ad0a1cedafff0db2cd
jmairena01 <jisaakm_1204@hotmail.com>
2021-08-20 15:32:24 -0600

    Se agrega menú, servicio y componente de clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ClientesService } from './clientes/clientes.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ClientesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/clientes/clientes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {Cliente} from './cliente';
+import { ClientesService } from './clientes.service';
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html'
+})
+export class ClientesComponent implements OnInit {
+ clientes: Cliente[] = [];
+  constructor(private clienteService: ClientesService) { }
+
+  ngOnInit(): void {
+  this.clientes = this.clienteService.getClientes();
+  }
+
+}
